7bf852c1c6f8863d245a0217cd5ab21ee766047f
Vishnu S <vishnu721@gmail.com>
2021-11-10 10:58:11 +0530

    added security group in node

--- Terraform/infra/jenkins-node/main.tf
+++ Terraform/infra/jenkins-node/main.tf
@@ -7,11 +7,21 @@ data "terraform_remote_state" "vpc_info" {
   }
 }
 
+data terraform_remote_state "jenkins-master" {
+    backend = "s3"
+    config = {      
+      bucket = "vishnu-remote"
+      key = "ec2/terraform.tfstate"
+      region = "us-east-1"
+     }
+}
+
 resource "aws_instance" "jenkins-node" {
   ami = "ami-0f8f3e38c86610f2c"
   instance_type          = "t2.micro"
   key_name               = "LinuxServer"
   vpc_security_group_ids = ["${data.terraform_remote_state.vpc_info.outputs.default_security_group_id}"]
+  security_groups = [ "jenkins-node-sg" ]
   subnet_id              = data.terraform_remote_state.vpc_info.outputs.public_subnet_ids[0]
   tags = {
     Terraform = "true"
@@ -19,17 +29,26 @@ resource "aws_instance" "jenkins-node" {
     Name      = "Jenkins-Node"
   }
 
-  provisioner "remote-exec" {
-    inline = ["echo Hello"]
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("./LinuxServer.pem")
-    }
-  }
 
   provisioner "local-exec" {
          command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key ./LinuxServer.pem -i '${aws_instance.jenkins-node.public_ip},' playbook.yml"
      }
 
+}
+
+resource "aws_security_group" "jenkins-node-sg" {
+  name = "jenkins-node-sg"
+  vpc_id = data.terraform_remote_state.vpc_info.outputs.vpc_id
+  ingress = [ {
+    cidr_blocks = [ "${data.terraform_remote_state.jenkins-master.outputs.public-ip}/32" ]
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+  } ]
+  egress = [ {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+  } ]
 }
